8d7935d66a4e5189e9d5e19c3e4c38cda4003516
Zakir Hussain <mzhussain092@gmial.com>
2022-09-21 21:25:10 +0530

    setup header comp

---
+++ src/components/Layout/Header.jsx
@@ -0,0 +1,26 @@
+// react imports
+import React, { Fragment } from "react";
+
+// Styles imports
+import styles from "./Header.module.css";
+
+// Asset imports
+import mealsImg from "../../assets/meals.jpg";
+import HeaderCartButton from "./HeaderCartButton";
+
+const Header = () => {
+  return (
+    <Fragment>
+      <header className={styles.header}>
+        <h1>InstaMeals</h1>
+        <HeaderCartButton />
+      </header>
+
+      <div className={styles["main-image"]}>
+        <img src={mealsImg} alt="food" />
+      </div>
+    </Fragment>
+  );
+};
+
+export default Header;
